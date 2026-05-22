35f6abd059bf49301dc469a48c683666d165243e
Infinity9888 <138257376+Infinity9888@users.noreply.github.com>
2026-05-22 15:29:45 +0300

    return early webhook

--- admin-panel/api/telegram-webhook.ts
+++ admin-panel/api/telegram-webhook.ts
@@ -4,6 +4,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  return res.status(200).send("EARLY OK");
+  
   try {
     /*
     if (!db) {
